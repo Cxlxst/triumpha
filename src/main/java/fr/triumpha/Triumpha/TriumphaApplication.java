183332f0e264649f8c20df50543755ca4b189f20
Cxlxst <celeste.terpin@outlook.com>
2024-04-16 17:24:42 +0200

    initialization

---
+++ src/main/java/fr/triumpha/Triumpha/TriumphaApplication.java
@@ -0,0 +1,13 @@
+package fr.triumpha.Triumpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriumphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriumphaApplication.class, args);
+	}
+
+}
