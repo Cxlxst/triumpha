0e8e0a7bed8229184c66ac4673f7f506a764f430
Cxlxst <celeste.terpin@outlook.com>
2024-04-16 17:33:12 +0200

    add imports

--- src/main/java/fr/triumpha/Triumpha/TriumphaApplication.java
+++ src/main/java/fr/triumpha/Triumpha/TriumphaApplication.java
@@ -2,8 +2,12 @@ package fr.triumpha.Triumpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class TriumphaApplication {
 
 	public static void main(String[] args) {
